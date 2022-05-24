e91286ce38ad7e25b68a180625e877a42f6cd6cf
MirkoLegnini <62476194+MirkoLegnini@users.noreply.github.com>
2022-05-24 15:35:46 +0200

    inizio scrittura test

--- code/model/Paziente.java
+++ code/model/Paziente.java
@@ -1,5 +1,121 @@
 package code.model;
 
+import java.time.LocalDate;
+
 public class Paziente {
     
+    int pazienteId;
+    String nome;
+    String cognome;
+    LocalDate dataNascita;
+    String telefono;
+    String mail;
+
+
+    public Paziente() {
+    }
+
+    public Paziente(String nome, String cognome, LocalDate dataNascita, String telefono, String mail) {
+        this.pazienteId = 1;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataNascita = dataNascita;
+        this.telefono = telefono;
+        this.mail = mail;
+    }
+
+    public int getPazienteId() {
+        return this.pazienteId;
+    }
+
+    public void setPazienteId(int pazienteId) {
+        this.pazienteId = pazienteId;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return this.cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public LocalDate getDataNascita() {
+        return this.dataNascita;
+    }
+
+    public void setDataNascita(LocalDate dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    public String getTelefono() {
+        return this.telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getMail() {
+        return this.mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Paziente pazienteId(int pazienteId) {
+        setPazienteId(pazienteId);
+        return this;
+    }
+
+    public Paziente nome(String nome) {
+        setNome(nome);
+        return this;
+    }
+
+    public Paziente cognome(String cognome) {
+        setCognome(cognome);
+        return this;
+    }
+
+    public Paziente dataNascita(LocalDate dataNascita) {
+        setDataNascita(dataNascita);
+        return this;
+    }
+
+    public Paziente telefono(String telefono) {
+        setTelefono(telefono);
+        return this;
+    }
+
+    public Paziente mail(String mail) {
+        setMail(mail);
+        return this;
+    }
+
+   
+
+    @Override
+    public String toString() {
+        return "{" +
+            " pazienteId='" + getPazienteId() + "'" +
+            ", nome='" + getNome() + "'" +
+            ", cognome='" + getCognome() + "'" +
+            ", dataNascita='" + getDataNascita() + "'" +
+            ", telefono='" + getTelefono() + "'" +
+            ", mail='" + getMail() + "'" +
+            "}";
+    }
+
+
+
 }
--- code/test/TestPaziente.java
+++ code/test/TestPaziente.java
@@ -1,11 +1,38 @@
 package code.test;
 import code.model.Paziente;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.*;
+
 public class TestPaziente{
+    
     Paziente paziente;
 
+        @Before
+        private void inizializza(){
 
-    @BeforeAll
-    
+            String nome= "Gaelia";
+            String cognome= "Mauro";
+            LocalDate dataNascita=LocalDate.of(2001, 9, 11);
+            String telefono="333 33 33 333";
+            String mail= "gaelia.mauro@google.com";
+            paziente=new Paziente(nome, cognome, dataNascita, telefono, mail);
+
+        }
+
+        @Test
+        private void testGetters(){
+            assertEquals(paziente.getNome(), "Gaelia");
+            assertEquals(paziente.getCognome(), "Mauro");
+            assertEquals(paziente.getDataNascita(), LocalDate.of(2001, 9, 11));
+            assertEquals(paziente.getTelefono(), "333 33 33 333");
+            assertEquals(paziente.getMail(), "gaelia.mauro@google.com");
+
+        }
+
 
     
 }
