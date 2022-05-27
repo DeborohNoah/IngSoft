e5fb006cc13f0cbbb2c04ded31a2211b8b66bd1f
MirkoLegnini <62476194+MirkoLegnini@users.noreply.github.com>
2022-05-27 13:52:12 +0200

    test setters scheda

--- code/test/TestPaziente.java
+++ code/test/TestPaziente.java
@@ -34,10 +34,11 @@ public class TestPaziente{
             float plicaSovrailliaca=5;
             float plicaAddominale=9;
             float plicaBicipitale=7;
-            float plicaQuadricipiatale=8;
+            float plicaQuadricipitale=8;
 
-            scheda=new Scheda(0, peso, circonferenzaVita, circonferenzaFianchi, plicaTricipitale, plicaSottoscapolare, 
-                                plicaSovrailliaca, plicaAddominale, plicaBicipitale, plicaQuadricipiatale, new HashMap<String, Path>(), new Dieta());
+            scheda=new Scheda(0, peso, circonferenzaVita, circonferenzaFianchi, plicaTricipitale,
+                                        plicaSottoscapolare, plicaSovrailliaca, plicaAddominale, plicaBicipitale,
+                                         plicaQuadricipitale, new HashMap<String, Path>(), new Dieta());
           
             storico.add(scheda);
             paziente=new Paziente(0, nome, cognome, dataNascita, telefono, mail, storico);
@@ -81,7 +82,9 @@ public class TestPaziente{
             assertEquals(scheda.getPlicaSovrailliaca(), 5);      
             assertEquals(scheda.getPlicaAddominale(), 9);
             assertEquals(scheda.getPlicaBicipitale(), 7);
-            assertEquals(scheda.getPlicaQuadricipiatale(), 8);       
+            assertEquals(scheda.getPlicaQuadricipitale(), 8);   
+            
+            
             float peso=100; 
             float circonferenzaVita=150;
             float circonferenzaFianchi=180;
@@ -90,24 +93,55 @@ public class TestPaziente{
             float plicaSovrailliaca=9;
             float plicaAddominale=2;
             float plicaBicipitale=1;
-            float plicaQuadricipiatale=20;
+            float plicaQuadricipitale=20;
 
+            scheda.setPeso(peso);
+            scheda.setCirconferenzaFianchi(circonferenzaFianchi);
+            scheda.setCirconferenzaVita(circonferenzaVita);
+            scheda.setPlicaTricipitale(plicaTricipitale);
+            scheda.setPlicaSottoscapolare(plicaSottoscapolare);
+            scheda.setPlicaSovrailliaca(plicaSovrailliaca);
+            scheda.setPlicaBicipitale(plicaBicipitale);
+            scheda.setPlicaAddominale(plicaAddominale);
+            scheda.setPlicaQuadricipitale(plicaQuadricipitale);
+
+            
+            assertEquals(scheda.getPeso(), 100);
+            assertEquals(scheda.getCirconferenzaVita(), 150);
+            assertEquals(scheda.getCirconferenzaFianchi(), 180);
+            assertEquals(scheda.getPlicaTricipitale(), 10);
+            assertEquals(scheda.getPlicaSottoscapolare(), 7);
+            assertEquals(scheda.getPlicaSovrailliaca(), 9);      
+            assertEquals(scheda.getPlicaAddominale(), 2);
+            assertEquals(scheda.getPlicaBicipitale(), 1);
+            assertEquals(scheda.getPlicaQuadricipitale(), 20); 
+
+            peso=70; 
+            circonferenzaVita=102;
+            circonferenzaFianchi=105;
+            plicaTricipitale=4;
+            plicaSottoscapolare=6;
+            plicaSovrailliaca=5;
+            plicaAddominale=9;
+            plicaBicipitale=7;
+            plicaQuadricipitale=8;
             Scheda secondaScheda = new Scheda(1, peso, circonferenzaVita, circonferenzaFianchi, plicaTricipitale, 
                                                 plicaSottoscapolare, plicaSovrailliaca, plicaAddominale, plicaBicipitale, 
-                                                plicaQuadricipiatale, new HashMap<String, Path>(), new Dieta());
+                                                plicaQuadricipitale, new HashMap<String, Path>(), new Dieta());
             
             paziente.getStorico().aggiungiScheda(secondaScheda);
             Scheda scheda2 = paziente.getStorico().getScheda(1);
 
-            assertEquals(scheda2.getPeso(), 100);
-            assertEquals(scheda2.getCirconferenzaVita(), 150);
-            assertEquals(scheda2.getCirconferenzaFianchi(), 180);
-            assertEquals(scheda2.getPlicaTricipitale(), 10);
-            assertEquals(scheda2.getPlicaSottoscapolare(), 7);
-            assertEquals(scheda2.getPlicaSovrailliaca(), 9);      
-            assertEquals(scheda2.getPlicaAddominale(), 2);
-            assertEquals(scheda2.getPlicaBicipitale(), 1);
-            assertEquals(scheda2.getPlicaQuadricipiatale(), 20); 
+            
+            assertEquals(scheda2.getPeso(), 70);
+            assertEquals(scheda2.getCirconferenzaVita(), 102);
+            assertEquals(scheda2.getCirconferenzaFianchi(), 105);
+            assertEquals(scheda2.getPlicaTricipitale(), 4);
+            assertEquals(scheda2.getPlicaSottoscapolare(), 6);
+            assertEquals(scheda2.getPlicaSovrailliaca(), 5);      
+            assertEquals(scheda2.getPlicaAddominale(), 9);
+            assertEquals(scheda2.getPlicaBicipitale(), 7);
+            assertEquals(scheda2.getPlicaQuadricipitale(), 8);  
         }
     
 }
